ab344da24a4ae1a5c144d25e7995a2cd85077400
sovisrushain <sovisrushain@gmail.com>
2022-07-22 01:17:12 +0530

    Add model Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import AddTodo from "./components/AddTodo";
 import NavBar from "./components/NavBar";
 
 const App = () => {
+
+  const TODO_LIST = [
+    new Todo('1', 'create react app'),
+    new Todo('2', 'create front end'),
+    new Todo('3', 'start spring backend')
+  ]
+
+  const [todoList, SetTodoList] = useState(TODO_LIST)
+
   return (
     <div className="container">
       <NavBar />
--- src/model/Todo.ts
+++ src/model/Todo.ts
@@ -9,4 +9,3 @@ class Todo {
 }
 
 export default Todo
-
