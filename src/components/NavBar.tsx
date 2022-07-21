a3e21a87757739c17636a7f686cf6614e668ea95
sovisrushain <sovisrushain@gmail.com>
2022-07-22 00:41:35 +0530

    Add navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import './navbar.css'
 
 const NavBar = () => {
     return(
-            <div className="navbar col-6 bg-info navbar-inverse" role="navigation">
+            <div className="navbar col-4 bg-info navbar-inverse" role="navigation">
                     <h2 className="navbar-header text-center">Todo App</h2>
             </div>
     )
